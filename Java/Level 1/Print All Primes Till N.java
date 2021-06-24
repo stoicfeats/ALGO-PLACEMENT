c1f020a3582e91568d9cf22cef32b6a3e56662d8
Yash Chaudhary <chaudharyyash088@gmail.com>
2021-06-24 23:55:23 +0530

    Update Print All Primes Till N.java

--- Java/Level 1/Print All Primes Till N.java
+++ Java/Level 1/Print All Primes Till N.java
@@ -2,6 +2,27 @@ import java.util.*;
 
 public class Main{
     public static void main(String[] args) {
-        // write your code here
+      Scanner scn = new Scanner(System.in);
+      
+      int low = scn.nextInt();
+      int hi = scn.nextInt();
+      
+      for(int div = low;div<=hi;div++){
+            
+            
+        int flag = 1;
+        
+        for(int j=2;j*j<=div;++j){
+            if(div%j==0){
+                flag=0;
+                break;
+            }
+        }
+        
+        if(flag == 1){
+            System.out.println(div);
+        }
+          
+      }
     }
 }
